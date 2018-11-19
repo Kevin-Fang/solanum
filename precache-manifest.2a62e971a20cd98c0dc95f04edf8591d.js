6ffa45aea7626919a61cd73fc520a81ae9593697
Kevin Fang <kfang1233@gmail.com>
2018-11-19 09:49:46 -0500

    Updates

---
+++ precache-manifest.2a62e971a20cd98c0dc95f04edf8591d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/solanum/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c03801045a6a3d6db1c7",
+    "url": "/solanum/static/js/runtime~main.c0380104.js"
+  },
+  {
+    "revision": "c607925b35038c660aa2",
+    "url": "/solanum/static/js/main.c607925b.chunk.js"
+  },
+  {
+    "revision": "f7662243ff5a0d0b5e7c",
+    "url": "/solanum/static/js/1.f7662243.chunk.js"
+  },
+  {
+    "revision": "c607925b35038c660aa2",
+    "url": "/solanum/static/css/main.988c272c.chunk.css"
+  },
+  {
+    "revision": "f64aba31b37f97ac7fc414af7a134d76",
+    "url": "/solanum/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4328198bdb3cd36df137209309a9be47.js"
+  "/solanum/precache-manifest.2a62e971a20cd98c0dc95f04edf8591d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/solanum/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
