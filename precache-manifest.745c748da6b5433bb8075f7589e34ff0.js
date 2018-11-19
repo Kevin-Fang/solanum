ce40d7ad4fd738dea0e8ac66a0e8ee7b064599cf
Kevin Fang <kfang1233@gmail.com>
2018-11-19 09:09:59 -0500

    Updates

---
+++ precache-manifest.745c748da6b5433bb8075f7589e34ff0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c625ab9836bb1e472d04",
+    "url": "/static/js/main.c625ab98.chunk.js"
+  },
+  {
+    "revision": "f7662243ff5a0d0b5e7c",
+    "url": "/static/js/1.f7662243.chunk.js"
+  },
+  {
+    "revision": "c625ab9836bb1e472d04",
+    "url": "/static/css/main.988c272c.chunk.css"
+  },
+  {
+    "revision": "2a2e544357fa4bceca7bc45a687879b1",
+    "url": "/index.html"
+  }
+];
